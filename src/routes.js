9c42a0ff89b4fe94a204f5a90497e8cd435c9910
Royal51651 <georgeeggers682@gmail.com>
2025-07-16 12:11:52 -0400

    Forgot to remove test file...

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 import Chat from "./lib/chat.svelte";
 import Settings from './lib/settings.svelte';
-import Test from './lib/test.svelte';
 export const routes = {
     "/": Chat,
-    "/settings": Settings,
-    "/test": Test
+    "/settings": Settings
 }
